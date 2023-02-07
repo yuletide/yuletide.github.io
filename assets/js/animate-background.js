d5cea6bfe2da03e2a1be99aa0a9781b60ec852bd
Alex Yule <adyule@gmail.com>
2023-02-07 12:51:06 -0800

    add color shifting for light mode also

--- assets/js/animate-background.js
+++ assets/js/animate-background.js
@@ -1,13 +1,36 @@
-const solarGreen = [0, 43, 54];
-const [red, green, blue] = solarGreen;
-const darkBlue = [0, 32, 44]
+// TODO: https://css-tricks.com/getting-javascript-to-talk-to-css-and-sass/ 
 const body = document.querySelector('body')
-const scale = 800;
+const solarGreenDark = [0, 43, 54];
+const solarMono3 = "#fdf6e3";
+const solarMono2 = "#93a1a1";
+const solarMono1 = "#93a1a1";
+const darkBlue = [0, 32, 44];
+const solarMix1 = "#c8ccc2";
+const solarMix2 = "#4683ab";
+const solarMix3 = "#04313c";
+const scale = 1200;
+let colorA, colorB;
 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  // console.log("dark mode")
+  colorA = solarGreenDark;
+  colorB = darkBlue;
+  // dark mode
+} else {
+  // console.log("light mode")
+  colorA = solarMono3;
+  colorB = solarMix1;
+
+}
+// minima does not swap themes automatically, only on page load
+// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+//   const newColorScheme = event.matches ? "dark" : "light";
+//   console.log("dark")
+// });
 window.addEventListener('scroll', () => {
   const y = (window.scrollY || window.pageYOffset) / scale;
-  const color = chroma.mix(solarGreen, darkBlue, y).css();
-  console.log(y, color);
+  const color = chroma.mix(colorA, colorB, y).css();
+  // console.log(y, color);
   body.style.backgroundColor = color;
   // const y = 1 + (window.scrollY || window.pageYOffset) / scale;
   // const yb = y*0.9;
